e4088f24ab4f823f5603af702e4fd6f9ff5beccf
Erran Carey <me@errancarey.com>
2014-01-21 14:04:13 -0600

    Add Controls::Client#update_security_controls

--- lib/controls/client/security_controls.rb
+++ lib/controls/client/security_controls.rb
@@ -22,6 +22,19 @@ module Controls
         end
       end
 
+      # Updates the specified security control(s)
+      #
+      # @param [Array[Hash{String=>String,Boolean}] controls a list of controls to update
+      # @return [void]
+      def update_security_controls(controls)
+        # [review] - this style is a discouraged for Arrays, but we want to treat controls as an Array of Hashes
+        if controls.is_a? Array
+          put '/security_controls', controls.to_json
+        elsif controls.is_a? Hash
+          put "/security_controls/#{control['name']}", control.to_json
+        end
+      end
+
       # @param [String] vector the threat vector to search for securuty controls
       #   by
       # @return [Array<Hash>] a list of hashes representing threats
