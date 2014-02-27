7e664114e52e942ba8147e1fd5d9831bfd6fc2a5
Erran Carey <me@errancarey.com>
2014-02-27 15:54:53 -0600

    Update gitignore

--- lib/controls/objects/coverage_information.rb
+++ lib/controls/objects/coverage_information.rb
@@ -1,7 +1,6 @@
 module Controls
   class CoverageInformation < Dish::Plate
     def to_s
-      # [todo] - generate this
       "#<#{self.class}: total: #{total}, covered: #{covered}, uncovered: #{uncovered}, percent_covered: #{percent_covered}>"
     end
   end
