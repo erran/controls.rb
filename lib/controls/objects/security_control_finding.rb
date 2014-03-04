d3874241b8b2d78ed8740c8b5d7da4381af7cfa8
Erran Carey <me@errancarey.com>
2014-03-03 22:03:29 -0600

    Update security_control_finding.rb

--- lib/controls/objects/security_control_finding.rb
+++ lib/controls/objects/security_control_finding.rb
@@ -3,10 +3,10 @@ require 'controls/objects/configuration_finding'
 module Controls
   class SecurityControlFinding < Dish::Plate
     coerce :assessmentTimestamp, ->(value) { Time.at(value / 1000) if value }
-    coerce :configurationFindings, Controls::ConfigurationFinding
+    coerce :findings, Controls::ConfigurationFinding
 
     def to_s
-      "#{security_control_name}: #{configuration_findings.map { |finding| "\n  #{finding}"  }.join}"
+      "#{name}: #{findings.map { |finding| "\n  #{finding}"  }.join}"
     end
   end
 end
