ec04903456942d30f58ea0c79f2f9d5d1f46528c
Erran Carey <me@errancarey.com>
2014-03-03 22:44:09 -0600

    Update findings.rb

--- lib/controls/client/findings.rb
+++ lib/controls/client/findings.rb
@@ -2,7 +2,8 @@ module Controls
   class Client
     # A module to encapsulate API methods related to assets
     # @since API v1.0
-    # @version v1.0.0
+    # [todo] - this version is obviously off
+    # @version v1.7.0
     module Findings
       # @!group Security Control/Configuration Findings Methods
 
