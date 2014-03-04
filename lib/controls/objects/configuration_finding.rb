6123c91f3852747edd35f8c19a847b00e705204a
Erran Carey <me@errancarey.com>
2014-03-03 22:06:26 -0600

    Update configuration_finding.rb

--- lib/controls/objects/configuration_finding.rb
+++ lib/controls/objects/configuration_finding.rb
@@ -1,9 +1,7 @@
 module Controls
   class ConfigurationFinding < Dish::Plate
-    coerce :assessmentTimestamp, ->(value) { Time.at(value / 1000) if value }
-
     def to_s
-      "#{state}: #{reason.strip}"
+      "#{name}: state: #{state} reason: #{reason.strip}"
     end
   end
 end
