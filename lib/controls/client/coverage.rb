3031c8c6d2564f37aec2657857ccfe41a7caa431
Erran Carey <me@errancarey.com>
2014-03-03 22:45:29 -0600

    Document Controls::Client::Coverage as per #3

--- lib/controls/client/coverage.rb
+++ lib/controls/client/coverage.rb
@@ -1,6 +1,14 @@
 module Controls
   class Client
+    # A module to encapsulate API methods related to coverage
+    # @since API v1.0
+    # [todo] - this version is obviously off
+    # @version v1.0.0
     module Coverage
+      # Either returns coverage for all security controls or one by name
+      #
+      # @param [String] security_control_name the security control to return coverage for
+      # @return [Array<Controls::SecurityControlCoverage>,Controls::SecurityControlCoverage]
       def security_control_coverage(security_control_name = nil)
         if security_control_name
           get "/coverage/security_controls/#{security_control_name}"
