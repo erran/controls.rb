e587459c58d53979d8036168d512d78929396add
Erran Carey <me@errancarey.com>
2014-06-16 23:41:50 -0500

    Update documentation in Controls::ConfigurationCoverage

--- lib/controls/objects/configuration_coverage.rb
+++ lib/controls/objects/configuration_coverage.rb
@@ -8,6 +8,11 @@ module Controls
     coerce :assessmentTimestamp, ->(value) { Time.at(value / 1000) if value }
     coerce :coverage, Controls::CoverageInformation
 
+    # Allows for comparison with other objects with coverage information
+    #
+    # @return [Fixnum] returns one of the following based on the percent of
+    #   assets that are covered -1 (less than `other`), 0 (equal to `other`),
+    #   or 1 (greater than `other`)
     def <=>(other)
       return unless other.respond_to? :coverage
       coverage.percent_covered <=> other.coverage.percent_covered
@@ -22,13 +27,21 @@ module Controls
       end
     end
 
+    # Converts the object into a {Controls::Configuration} by name and
+    # whether it is enabled/disabled
+    #
+    # @return {Controls::Configuration}
     def without_coverage
       Controls::Configuration.new(enabled: enabled, name: name)
     end
 
+    # Removes the coverage from the {ConfigurationCoverage} object, making it
+    # equivalent to a {Configuration} in terms of duck-typing
+    #
+    # @return {Controls::Configuration}
     def without_coverage!
       @_original_hash.delete_if do |key, _value|
-        not %w[enabled name].include?(key)
+        !%w(enabled name).include?(key)
       end
     end
 
